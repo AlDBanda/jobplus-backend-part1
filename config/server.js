11cd6d96f39a21fc19485bc4f86887b5d3d325f3
ALDBANDA <alfred.banda1986@gmail.com>
2023-09-07 10:13:52 +0100

    'error html css'

--- config/server.js
+++ config/server.js
@@ -1,8 +1,11 @@
+const { v4: uuidv4 } = require('uuid');
+
+
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array('APP_KEYS', [uuidv4(), uuidv4()]),
   },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
