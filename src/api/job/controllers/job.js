090c0d5c0345a5d896b1e13e66d46fe190a12e56
ALDBANDA <alfred.banda1986@gmail.com>
2023-10-31 09:38:53 +0000

    'controllers'

--- src/api/job/controllers/job.js
+++ src/api/job/controllers/job.js
@@ -4,7 +4,13 @@
  * job controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::job.job');
-
+module.exports = ({ strapi }) => ({
+  async find (ctx) {
+    console.log('ctx', ctx);
+     try {
+      ctx.body  = 'Hello World';
+     } catch ( error ) {
+      strapi.log.error(error)
+     }
+  },
+})
