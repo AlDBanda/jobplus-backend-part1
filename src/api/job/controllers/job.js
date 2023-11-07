076c6b815483b4524f8f5b027d9883f19bd95221
ALDBANDA <alfred.banda1986@gmail.com>
2023-11-07 13:09:09 +0000

    'Controller update'

--- src/api/job/controllers/job.js
+++ src/api/job/controllers/job.js
@@ -6,9 +6,8 @@
 
 module.exports = ({ strapi }) => ({
   async find (ctx) {
-    console.log('ctx', ctx);
      try {
-      ctx.body  = 'Hello World';
+      ctx.body  = await strapi.service("api::job.job").find(ctx.query)
      } catch ( error ) {
       strapi.log.error(error)
      }
