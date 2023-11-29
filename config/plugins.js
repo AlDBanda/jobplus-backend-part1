9a9847f0c982691c15c019b2333b5d39b12b8950
ALDBANDA <alfred.banda1986@gmail.com>
2023-11-29 12:38:43 +0000

    'yarn'

--- config/plugins.js
+++ config/plugins.js
@@ -3,14 +3,14 @@ module.exports = ({ env }) => ({
 
   email: {
     config: {
-      provider: 'sendgrid', // For community providers pass the full package name (e.g. provider: 'strapi-provider-email-mandrill')
+      provider: 'strapi-provider-email-postmark', // For community providers pass the full package name (e.g. provider: 'strapi-provider-email-mandrill')
       providerOptions: {
-        apiKey: env('SENDGRID_API_KEY'),
+        apiKey: env('POSTMARK_API_KEY'),
       },
       settings: {
-        defaultFrom: env('SENDGRID_FROM_EMAIL'),
-        defaultReplyTo: env('SENDGRID_FROM_EMAIL'),
-        testAddress: env('SENDGRID_FROM_EMAIL'),
+        defaultFrom: env('POSTMARK_FROM_EMAIL'),
+        defaultReplyTo: env('POSTMARK_FROM_EMAIL'),
+        testAddress: env('POSTMARK_FROM_EMAIL'),
       },
     },
   },
